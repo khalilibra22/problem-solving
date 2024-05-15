7a038e2e6b2f69728018d45eefac788575e63544
DOUIS Ibrahim Khalil <66187505+khalilibra22@users.noreply.github.com>
2024-05-15 02:07:41 +0200

    Create json_with_javascript.js

---
+++ json_with_javascript.js
@@ -0,0 +1,48 @@
+const classroom = {
+  students_number: null,
+  total_average: null,
+  students: [
+    {
+      id: 1,
+      fullname: "John Doe",
+      grades: [90, 95, 80],
+      average: null,
+    },
+    {
+      id: 2,
+      fullname: "Jane Smith",
+      grades: [85, 90, 92],
+      average: null,
+    },
+    {
+      id: 3,
+      fullname: "Mike Johnson",
+      grades: [70, 75, 68],
+      average: null,
+    },
+  ],
+};
+
+function displayStudentInfo() {
+  //Create function to display student infomation (id, fullname, grades, average)
+  //Use for loop through the students array
+  //Display the student information in the console using console.log()
+  //Display classroom students number in the console using console.log()
+  //Display classroom total average in the console using console.log()
+  //YOUR CODE HERE
+
+  
+}
+
+function calculateAverage() {
+  //Create function to calculate average grade for each student
+  //Add the average grade to the student average property
+  //Calculate classroom students number and it to students_number property
+  //Calculate classroom total average and it to total_average property
+  //YOUR CODE HERE
+ 
+}
+
+// Call the functions
+calculateAverage();
+displayStudentInfo();
